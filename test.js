3f531e024327a4c8902e9fd7e0410fa2979a3eac
fireattack <human.peng@gmail.com>
2018-01-08 00:41:15 -0600

    Better format

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 if (chrome) browser = chrome;
 browser.webRequest.onHeadersReceived.addListener(function(e) {
     for (header of e.responseHeaders) {
-        if (header.name.toLowerCase() === 'content-disposition') {            
+        if (header.name.toLowerCase() === 'content-disposition') {
             console.log(header.value);
             let str = '英文3.torrent';
-           // header.value = `attachment; filename="${str}"`; //Doesn't work
-            header.value = `attachment; filename*=UTF-8''${encodeURIComponent(str)}`
-        //    header.value = `attachment; filename="${unescape(encodeURIComponent(str))}"`; // Works
+            // header.value = `attachment; filename="${str}"`; //Chrome only
+            // header.value = `attachment; filename="${unescape(encodeURIComponent(str))}"`; //Firefox only
+            header.value = `attachment; filename*=UTF-8''${encodeURIComponent(str)}` //Works for both
         }
-    }	
+    }
 	return {"responseHeaders": e.responseHeaders};
 }, {urls: ["<all_urls>"]}, ['blocking', 'responseHeaders']);
